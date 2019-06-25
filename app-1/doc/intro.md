cd2bb317e6e9e125c9c47daf88386e60c2636a16
Nils Blum-Oeste <nils@pitch.io>
2019-06-25 09:21:07 +0200

    Initial commit

---
+++ app-1/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to app-1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ shared/test/shared/core_test.clj
@@ -0,0 +1,7 @@
+(ns shared.core-test
+  (:require [clojure.test :refer :all]
+            [shared.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
